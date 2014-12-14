928ecf1d0dbe547c406eb359e35f90119bf7f27e
Vitaliy Orlov <vitaliy@madebyewave.com>
2014-12-14 09:53:23 +0200

    Functionality to generate passwords added

--- scripts/prj.php
+++ scripts/prj.php
@@ -6,7 +6,7 @@ define('PRJ_DOMAIN_PREFIX', '.sv');
 define('EDITOR_PATH', 'C:\Program Files (x86)\phpDesigner 8\phpDesigner.exe');
 define('NOTEPAD_PATH', 'C:\Program Files (x86)\Notepad++\notepad++.exe');
 define('WIN_HOSTS_PATH', 'C:\Windows\system32\drivers\etc\hosts');
-define('MYSQL_CONF_PATH', 'D:\Progr\Servers\WebAMP\MySQL Server 5.1\my.ini');
+define('MYSQL_CONF_PATH', 'D:\Progr\WebAMP\MySQL Server 5.1\my.ini');
 define('VHOSTS_CONF_PATH', 'D:\Progr\WebAMP\Apache2.2\conf\extra\httpd-vhosts.conf');
 
 function callback($cmd)
@@ -57,6 +57,18 @@ function getProjectPath($prjArgument) {
 	return $ret;
 }
 
+function generatePassword($length = 8) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $count = mb_strlen($chars);
+
+    for ($i = 0, $result = ''; $i < $length; $i++) {
+        $index = rand(0, $count - 1);
+        $result .= mb_substr($chars, $index, 1);
+    }
+
+    return $result;
+}
+
 if ($arg = parseArguments() AND is_array($arg))
 {
 	switch ($arg[0]) 
@@ -95,10 +107,14 @@ if ($arg = parseArguments() AND is_array($arg))
 			echo '-    prj ls [project name] = show projects list'.PHP_EOL.PHP_EOL;
 			echo '-    prj cd [project name] = change dir in console to project dir'.PHP_EOL.PHP_EOL;
 			echo '-    prj open [project name] = open project folder in explorer'.PHP_EOL.PHP_EOL;
-			echo '-    prj edit [hosts|vhosts|mysql] = open specified config in editor'.PHP_EOL.PHP_EOL;
-			echo '-    prj find [keyword] (!google[!php|!mysql]) = open browser with search page for specified keyword. You can also specify search site google, php or mysql.'.PHP_EOL.PHP_EOL;			
+			echo '-    prj edit [hosts|vhosts|mysql|source] = open specified config in editor'.PHP_EOL.PHP_EOL;
+			echo '-    prj find [keyword] ([!google|!image|!php|!mysql]) = open browser with search page for specified keyword. You can also specify search site google, php or mysql. Default search engine: !google'.PHP_EOL.PHP_EOL;			
+			echo '-    prj md5 [keyword] = show md5 of keyword'.PHP_EOL.PHP_EOL;
+			echo '-    prj ip = show ip information'.PHP_EOL.PHP_EOL;
+			echo '-    prj pass ([length]) = generate random password. Optional parameter is password length. Default length 10.'.PHP_EOL.PHP_EOL;
 			echo '-    prj help = show commands list'.PHP_EOL.PHP_EOL;
 			echo '-    prj ver = show version'.PHP_EOL.PHP_EOL;
+
 		break;			
 		case 'ver':
 			echo 'Project manager, v.2.0'.PHP_EOL;
@@ -116,14 +132,17 @@ if ($arg = parseArguments() AND is_array($arg))
 						echo 'Undefined config';
 					break;
 					case 'hosts': 
-						callback('"'.NOTEPAD_PATH.'" "'.WIN_HOSTS_PATH.'"');
+						call('"'.NOTEPAD_PATH.'" "'.WIN_HOSTS_PATH.'"');
 					break;
 					case 'mysql': 
-						callback('"'.NOTEPAD_PATH.'" "'.MYSQL_CONF_PATH.'"');
+						call('"'.NOTEPAD_PATH.'" "'.MYSQL_CONF_PATH.'"');
 					break;
 					case 'vhosts': 
-						callback('"'.NOTEPAD_PATH.'" "'.VHOSTS_CONF_PATH.'"');
+						call('"'.NOTEPAD_PATH.'" "'.VHOSTS_CONF_PATH.'"');
 					break;
+					case 'source': 
+						call('"'.NOTEPAD_PATH.'" "'.__FILE__.'"');
+					break;					
 				}
 			}
 		break;
@@ -131,28 +150,31 @@ if ($arg = parseArguments() AND is_array($arg))
 			if (!isset($arg[1])) {
 				echo 'ERR: Search keyword not found';
 			} else {
-				$searchEngine = $arg[count($arg)-1];
-				if (!in_array($searchEngine,array('php!','google!','mysql!','!php','!google','!mysql'))) {
-					$searchEngine = 'google!';
+				if (strpos($arg[count($arg)-1],'!')!==FALSE) {
+					$searchEngine = str_replace('!','',$arg[count($arg)-1]);
+					if (!in_array($searchEngine,array('php','google','mysql','image','images'))) {
+						$searchEngine = 'google';
+					} else {
+						unset($arg[count($arg)-1]);
+					}
 				} else {
-					$searchEngine = $arg[count($arg)-1];
-					unset($arg[count($arg)-1]);
+					$searchEngine = 'google';
 				}
 				
 				$query = implode(' ',array_slice($arg, 1));
 
 				switch($searchEngine) {
 					default:
-					case '!google':
-					case 'google!':
+					case 'google':
 						$url = 'https://www.google.com/search?ie=utf-8&oe=utf-8&q='.urlencode($query);
 					break;
-					case '!php':
-					case 'php!':
+					case 'image':
+						$url = 'https://www.google.com/search?safe=off&tbm=isch&q='.urlencode($query);
+					break;					
+					case 'php':
 						$url = 'http://php.net/search.php?show=quickref&pattern='.urlencode($query);
 					break;
-					case '!mysql':
-					case 'mysql!':
+					case 'mysql':
 						$url = 'https://search.oracle.com/search/search?group=MySQL&q='.urlencode($query);
 					break;
 				}
@@ -165,12 +187,20 @@ if ($arg = parseArguments() AND is_array($arg))
 			if (!isset($arg[1])) {
 				echo 'ERR: keyword not found';
 			} else {
-				echo md5(trim($arg[1])).PHP_EOL;
+				$str = trim($argv[1]);
+				$str = trim(preg_replace('~^'.preg_quote($arg[0]).'~','',$str));
+				echo md5($str).PHP_EOL;
 			}			
 		break;
 
 		case 'ip':
 			echo 'EXTERNAL IP: '.file_get_contents('http://phihag.de/ip/').PHP_EOL;
+		break;
+		
+		case 'pass':
+			$length = 10;
+			if (isset($arg[1]) AND intval($arg[1])>0) $length = intval($arg[1]);
+			echo generatePassword($length).PHP_EOL;
 		break;
 	}
 } else {
